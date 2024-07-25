c628c5d49b59e2d3d11a0d417d2b2e4f3c7410e5
fzentenomaier <fzenteno.maier@gmail.com>
2024-07-26 00:01:03 +0800

    Update stocks_portfolio.py

--- stocks_portfolio.py
+++ stocks_portfolio.py
@@ -8,7 +8,6 @@ import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 from plotly.offline import init_notebook_mode
-init_notebook_mode(connected=True)
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from plotly.subplots import make_subplots
